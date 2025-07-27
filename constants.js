6244130277e16edab466fb9f991435783fef002d
Diego <ecua_soul@yahoo.com>
2025-07-26 19:14:27 -0800

    chore: Added configuration and constants

---
+++ constants.js
@@ -0,0 +1,27 @@
+export const CONFIG = {
+  MAX_INPUT_LENGTH: 15,
+  SIGNIFICANT_DIGITS: 12,
+  EXPONENTIAL_THRESHHOLD: 100,
+  EXPONENTIAL_MIN: 0.000001,
+  EXPONENTIAL_PRECISION: 6,
+};
+
+export const OPERATIONS = {
+  ADD: "+",
+  SUBSTRACT: "-",
+  MULTIPLY: "×",
+  DIVIDE: "÷",
+};
+
+export const KEYS = {
+  ENTER: "Enter",
+  EXCAPE: "Escape",
+  BACKSPACE: "Backspace",
+  DECIMAL: ".",
+  EQUALS: "=",
+};
+
+export const ERROR_MESSAGES = {
+  DIVISION_BY_ZERO: "Error",
+  INVALID_INPUT: "Error",
+};
