d5db756dba3a8c82f739ee4e3ab15d27ba8dce2b
DD <ecua_soul@yahoo.com>
2025-07-28 01:40:15 -0500

    chore: Removed unused error message constants

--- constants.js
+++ constants.js
@@ -1,27 +1,11 @@
-export const CONFIG = {
-  MAX_INPUT_LENGTH: 15,
-  SIGNIFICANT_DIGITS: 12,
-  EXPONENTIAL_THRESHHOLD: 100,
-  EXPONENTIAL_MIN: 0.000001,
-  EXPONENTIAL_PRECISION: 6,
-};
-
 export const OPERATIONS = {
   ADD: "+",
-  SUBSTRACT: "-",
+  SUBTRACT: "-",
   MULTIPLY: "×",
   DIVIDE: "÷",
 };
 
-export const KEYS = {
-  ENTER: "Enter",
-  EXCAPE: "Escape",
-  BACKSPACE: "Backspace",
-  DECIMAL: ".",
-  EQUALS: "=",
-};
-
 export const ERROR_MESSAGES = {
-  DIVISION_BY_ZERO: "Error",
+  DIVISION_BY_ZERO: "Cannot divide by zero!",
   INVALID_INPUT: "Error",
 };
