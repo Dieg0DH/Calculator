3bd12fd5acb7eddd21888d00a4b68dce4601704a
DD <ecua_soul@yahoo.com>
2025-08-02 13:14:58 -0500

    feat: add the .active-by-keyboard class on keydown

--- ui.js
+++ ui.js
@@ -39,39 +39,98 @@ export class CalculatorUI {
 
   setupKeyboard() {
     document.addEventListener("keydown", (e) => {
+      let button = null;
+
       if (e.key >= "0" && e.key <= "9") {
+        button = Array.from(document.querySelectorAll("[data-number]")).find(
+          (btn) => btn.innerText === e.key
+        );
         this.calculator.appendNumber(e.key);
-        return;
+      } else {
+        switch (e.key) {
+          case ".":
+            button = document.querySelector("[data-decimal]");
+            this.calculator.appendNumber(".");
+            break;
+          case "+":
+            button = document.querySelector('[data-operation="+"]');
+            this.calculator.setOperation(OPERATIONS.ADD);
+            break;
+          case "-":
+            button = document.querySelector('[data-operation="-"]');
+            this.calculator.setOperation(OPERATIONS.SUBTRACT);
+            break;
+          case "*":
+            button = document.querySelector('[data-operation="×"]');
+            this.calculator.setOperation(OPERATIONS.MULTIPLY);
+            break;
+          case "/":
+            e.preventDefault();
+            button = document.querySelector('[data-operation="÷"]');
+            this.calculator.setOperation(OPERATIONS.DIVIDE);
+            break;
+          case "Enter":
+          case "=":
+            e.preventDefault();
+            button = document.querySelector("[data-equals]");
+            this.calculator.compute();
+            break;
+          case "Backspace":
+            button = document.querySelector("[data-delete]");
+            this.calculator.delete();
+            break;
+          case "Escape":
+            button = document.querySelector("[data-clear]");
+            this.calculator.clear();
+            break;
+        }
       }
 
-      switch (e.key) {
-        case ".":
-          this.calculator.appendNumber(".");
-          break;
-        case "+":
-          this.calculator.setOperation(OPERATIONS.ADD);
-          break;
-        case "-":
-          this.calculator.setOperation(OPERATIONS.SUBTRACT);
-          break;
-        case "*":
-          this.calculator.setOperation(OPERATIONS.MULTIPLY);
-          break;
-        case "/":
-          e.preventDefault();
-          this.calculator.setOperation(OPERATIONS.DIVIDE);
-          break;
-        case "Enter":
-        case "=":
-          e.preventDefault();
-          this.calculator.compute();
-          break;
-        case "Backspace":
-          this.calculator.delete();
-          break;
-        case "Escape":
-          this.calculator.clear();
-          break;
+      // Active class
+      if (button) {
+        button.classList.add("active-by-keyboard");
+      }
+    });
+
+    document.addEventListener("keyup", (e) => {
+      let button = null;
+
+      if (e.key >= "0" && e.key <= "9") {
+        button = Array.from(document.querySelectorAll("[data-number]")).find(
+          (btn) => btn.innerText === e.key
+        );
+      } else {
+        switch (e.key) {
+          case ".":
+            button = document.querySelector("[data-decimal]");
+            break;
+          case "+":
+            button = document.querySelector('[data-operation="+"]');
+            break;
+          case "-":
+            button = document.querySelector('[data-operation="-"]');
+            break;
+          case "*":
+            button = document.querySelector('[data-operation="×"]');
+            break;
+          case "/":
+            button = document.querySelector('[data-operation="÷"]');
+            break;
+          case "Enter":
+          case "=":
+            button = document.querySelector("[data-equals]");
+            break;
+          case "Backspace":
+            button = document.querySelector("[data-delete]");
+            break;
+          case "Escape":
+            button = document.querySelector("[data-clear]");
+            break;
+        }
+      }
+
+      if (button) {
+        button.classList.remove("active-by-keyboard");
       }
     });
   }
